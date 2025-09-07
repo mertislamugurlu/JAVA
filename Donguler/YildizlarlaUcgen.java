468a4fbec209ce336902366d4cce764788577873
Mert İslam Uğurlu <mertislamugurlu@gmail.com>
2025-09-07 07:53:30 +0300

    Add files via upload

---
+++ Donguler/YildizlarlaUcgen.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class YildizlarlaUcgen {
+    public static void main(String[] args) {
+        int yildiz = 0, girilensayi, topla = 0;
+        System.out.print("Adım sayısını giriniz: ");
+        Scanner input = new Scanner(System.in);
+        yildiz = input.nextInt();
+        girilensayi = yildiz;
+
+
+        // İlk öncelikle girilen adım sayısı
+        // değerini bulmak istiyorum buna göre yildiz çizdireceğim.
+
+        for (int i = 1; i <= girilensayi; i++) {
+            for (int j = 1; j <= (girilensayi - i); j++) { //girilen sayı -1 boşluk
+                System.out.print(" ");
+
+
+            }
+
+            for (int j = 0; j <= (2 * i- 1) ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+
+    }
+}
